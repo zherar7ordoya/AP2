ed61cc13a61a2cf2f8e1c27e113b60a69be9a48e
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-04-22 18:12:04 -0300

    Visual Studio

---
+++ CSharp/TP01-Laboratorio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP01_Laboratorio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            float b, c;
+            a = 10;
+            b = (float)30.5;
+            c = (float)(a + b);
+            Console.WriteLine("{0} + {1} = {2}", a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
