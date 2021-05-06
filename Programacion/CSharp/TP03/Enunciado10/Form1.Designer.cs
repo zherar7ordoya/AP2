5251d6f3a80713659e8efa3ee0fd37c46aeec417
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-05-06 00:47:51 -0300

    Kelvin

--- Programacion/CSharp/TP03/Enunciado10/Form1.Designer.cs
+++ Programacion/CSharp/TP03/Enunciado10/Form1.Designer.cs
@@ -51,35 +51,50 @@
       // comboBox1
       // 
       this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin",
+            "Rankine"});
       this.comboBox1.Location = new System.Drawing.Point(240, 12);
       this.comboBox1.Name = "comboBox1";
       this.comboBox1.Size = new System.Drawing.Size(75, 21);
       this.comboBox1.TabIndex = 8;
       this.comboBox1.Text = "Desde";
+      this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
       // 
       // comboBox2
       // 
       this.comboBox2.FormattingEnabled = true;
+      this.comboBox2.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin",
+            "Rankine"});
       this.comboBox2.Location = new System.Drawing.Point(321, 12);
       this.comboBox2.Name = "comboBox2";
       this.comboBox2.Size = new System.Drawing.Size(75, 21);
       this.comboBox2.TabIndex = 9;
       this.comboBox2.Text = "Hacia";
+      this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
       // 
       // textBox1
       // 
       this.textBox1.Location = new System.Drawing.Point(184, 12);
       this.textBox1.Name = "textBox1";
       this.textBox1.Size = new System.Drawing.Size(50, 20);
-      this.textBox1.TabIndex = 10;
+      this.textBox1.TabIndex = 1;
       this.textBox1.Text = "Cantidad";
+      this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
       // 
       // label1
       // 
       this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.ForeColor = System.Drawing.Color.Red;
       this.label1.Location = new System.Drawing.Point(402, 15);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(68, 13);
+      this.label1.Size = new System.Drawing.Size(80, 13);
       this.label1.TabIndex = 11;
       this.label1.Text = "Equivalencia";
       // 
--- Programacion/CSharp/TP03/Enunciado10/Form1.cs
+++ Programacion/CSharp/TP03/Enunciado10/Form1.cs
@@ -13,11 +13,14 @@ namespace Enunciado10 {
     public Form1() {
       InitializeComponent();
     }
+    // *------------------------------------------------------------=> GLOBALES
+    string texto, desde, hacia;
+    int numero;
 
     private void Form1_Load(object sender, EventArgs e) {
       // *-----------------------------------------------------------------=> *
       label0.Text = "PORTADA.------------" + Environment.NewLine +
-                    "2021-05-05" + Environment.NewLine +
+                    "2021-05-06" + Environment.NewLine +
                     "Gerardo Tordoya" + Environment.NewLine +
                     "Trabajo Práctico 3" + Environment.NewLine +
                     "Programación I" + Environment.NewLine +
@@ -28,6 +31,64 @@ namespace Enunciado10 {
       this.MaximizeBox = false;
       this.MinimizeBox = false;
       // *-----------------------------------------------------------------=> *
+    }
+    // *-------------------------------------------------------------=> EVENTOS
+    private void textBox1_TextChanged(object sender, EventArgs e) {
+      convertir();
+    }
+    private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+      convertir();
+    }
+    private void comboBox2_SelectedIndexChanged(object sender, EventArgs e) {
+      convertir();
+    }
+    // *-----------------------------------------------------------=> PRINCIPAL
+    private void convertir() {
+      // Verificación.
+      texto = textBox1.Text;
+      desde = comboBox1.Text;
+      hacia = comboBox2.Text;
+      bool correcto1 = Int32.TryParse(texto, out numero);
+
+      bool correcto2 = false, correcto3 = false;
+      if (desde != "Desde") correcto2 = true;
+      if (hacia != "Hacia") correcto3 = true;
+
+      // Operaciones.
+      if (correcto1 && correcto2 && correcto3) {
+        calcular();
+        grabar();
+      }
+    }
+
+    private void calcular() {
+      if (desde == "Celsius" && hacia == "Celsius") label1.Text = (numero * 1).ToString();
+      if (desde == "Celsius" && hacia == "Fahrenheit") label1.Text = (numero * 1.8 + 32).ToString();
+      if (desde == "Celsius" && hacia == "Kelvin") label1.Text = (numero + 273.15).ToString();
+      if (desde == "Celsius" && hacia == "Rankine") label1.Text = (numero * 1.8 + 32 + 459.67).ToString();
+
+      if (desde == "Fahrenheit" && hacia == "Fahrenheit") label1.Text = (numero * 1).ToString();
+      if (desde == "Fahrenheit" && hacia == "Celsius") label1.Text = ((numero - 32) / 1.8).ToString();
+      if (desde == "Fahrenheit" && hacia == "Kelvin") label1.Text = ((numero + 459.67) / 1.8).ToString();
+      if (desde == "Fahrenheit" && hacia == "Rankine") label1.Text = (numero + 459.67).ToString();
+
+      if (desde == "Kelvin" && hacia == "Kelvin") label1.Text = (numero * 1).ToString();
+      if (desde == "Kelvin" && hacia == "Celsius") label1.Text = (numero - 273.15).ToString();
+      if (desde == "Kelvin" && hacia == "Fahrenheit") label1.Text = (numero * 1.8 - 459.67).ToString();
+      if (desde == "Kelvin" && hacia == "Rankine") label1.Text = (numero * 1.8).ToString();
+
+      if (desde == "Rankine" && hacia == "Rankine") label1.Text = (numero * 1).ToString();
+      if (desde == "Rankine" && hacia == "Celsius") label1.Text = ((numero - 32 - 459.67) / 1.8).ToString();
+      if (desde == "Rankine" && hacia == "Fahrenheit") label1.Text = (numero - 459.67).ToString();
+      if (desde == "Rankine" && hacia == "Kelvin") label1.Text = (numero / 1.8).ToString();
+    }
+
+    private void grabar() {
+      var index = dataGridView1.Rows.Add();
+      dataGridView1.Rows[index].Cells["Cantidad"].Value = textBox1.Text;
+      dataGridView1.Rows[index].Cells["Desde"].Value = comboBox1.Text;
+      dataGridView1.Rows[index].Cells["Hacia"].Value = comboBox2.Text;
+      dataGridView1.Rows[index].Cells["Equivalencia"].Value = label1.Text;
     }
   }
 }
