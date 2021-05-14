d58729b70d83d7746e1fead8c570b2adb1235410
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-05-14 00:14:08 -0300

    the fox jumps over the dog

--- Programacion/CSharp/TP04/_LinkedList/Program.cs
+++ Programacion/CSharp/TP04/_LinkedList/Program.cs
@@ -7,7 +7,7 @@ using System.Text;
  * Esto es lo primero y más básico que habría que responder:
  * Esta estructura de datos, ¿para qué sirve, para qué se usa?
  * USOS:
- * -) (No lo especifica el artículo). => https://dev.to/adavidoaiei/fundamental-data-structures-and-algorithms-in-c-4ocf
+ * -) (No lo especifica el artículo). 
  * */
 
 namespace _LinkedList {
@@ -15,67 +15,68 @@ namespace _LinkedList {
     static void Main(string[] args) {
 
       // Create the link list.
-      string[] palabras =
-          { "the", "fox", "jumps", "over", "the", "dog" };
+      string[] palabras = { "Uno", "mas", "Dos", "mas", "Tres", "mas" };
       LinkedList<string> oracion = new LinkedList<string>(palabras);
-      Display(oracion, "The linked list values:");
-      Console.WriteLine("oracion.Contains(\"jumps\") = {0}", oracion.Contains("jumps"));
+
+      Muestrame(oracion, "\nValores de la lista enlazada:");
+      Console.WriteLine("oracion.Contains(\"Gerardo\") = {0}", oracion.Contains("Gerardo"));
+      Console.WriteLine("oracion.Contains(\"Cuatro\") = {0}", oracion.Contains("Cuatro"));
 
       // Add the word 'today' to the beginning of the linked list.
-      oracion.AddFirst("today");
-      Display(oracion, "Test 1: Add 'today' to beginning of the list:");
+      oracion.AddFirst("Cero");
+      Muestrame(oracion, "\nPrueba 1: Añadir 'Cero' al principio de la lista enlazada:");
 
       // Move the first node to be the last node.
       LinkedListNode<string> nodo1 = oracion.First;
       oracion.RemoveFirst();
       oracion.AddLast(nodo1);
-      Display(oracion, "Test 2: Move first node to be last node:");
+      Muestrame(oracion, "\nPrueba 2: Mover el primer nodo a la última posición:");
 
       // Change the last node to 'yesterday'.
       oracion.RemoveLast();
-      oracion.AddLast("yesterday");
-      Display(oracion, "Test 3: Change the last node to 'yesterday':");
+      oracion.AddLast("Siete");
+      Muestrame(oracion, "\nPrueba 3: Cambiar el último nodo a 'Siete':");
 
       // Move the last node to be the first node.
       nodo1 = oracion.Last;
       oracion.RemoveLast();
       oracion.AddFirst(nodo1);
-      Display(oracion, "Test 4: Move last node to be first node:");
+      Muestrame(oracion, "\nPrueba 4: Mover el último nodo al primer lugar:");
 
       // Indicate the last occurence of 'the'.
       oracion.RemoveFirst();
-      LinkedListNode<string> nodoActual = oracion.FindLast("the");
-      IndicateNode(nodoActual, "Test 5: Indicate last occurence of 'the':");
+      LinkedListNode<string> nodoActual = oracion.FindLast("es");
+      BuscarNodo(nodoActual, "\nPrueba 5: Indicar la última posición de 'es':");
 
       // Add 'lazy' and 'old' after 'the' (the LinkedListNode named nodoActual).
-      oracion.AddAfter(nodoActual, "old");
-      oracion.AddAfter(nodoActual, "lazy");
-      IndicateNode(nodoActual, "Test 6: Add 'lazy' and 'old' after 'the':");
+      oracion.AddAfter(nodoActual, "Def");
+      oracion.AddAfter(nodoActual, "Leppard");
+      BuscarNodo(nodoActual, "\nPrueba 6: Añadir 'Def' y 'Leppard' despues de 'es':");
 
       // Indicate 'fox' node.
-      nodoActual = oracion.Find("fox");
-      IndicateNode(nodoActual, "Test 7: Indicate the 'fox' node:");
+      nodoActual = oracion.Find("Tordoya");
+      BuscarNodo(nodoActual, "\nPrueba 7: Indicar el nodo 'Tordoya' node:");
 
       // Add 'quick' and 'brown' before 'fox':
-      oracion.AddBefore(nodoActual, "quick");
-      oracion.AddBefore(nodoActual, "brown");
-      IndicateNode(nodoActual, "Test 8: Add 'quick' and 'brown' before 'fox':");
+      oracion.AddBefore(nodoActual, "Hysteria");
+      oracion.AddBefore(nodoActual, "Animal");
+      BuscarNodo(nodoActual, "\nPrueba 8: Añadir 'Hysteria' y 'Animal' antes de 'es':");
 
       // Keep a reference to the nodoActual node, 'fox',
       // and to the previous node in the list. Indicate the 'dog' node.
       nodo1 = nodoActual;
-      LinkedListNode<string> mark2 = nodoActual.Previous;
-      nodoActual = oracion.Find("dog");
-      IndicateNode(nodoActual, "Test 9: Indicate the 'dog' node:");
+      LinkedListNode<string> nodo2 = nodoActual.Previous;
+      nodoActual = oracion.Find("Def");
+      BuscarNodo(nodoActual, "\nPrueba 9: Indicar el nodo 'Def':");
 
       // The AddBefore method throws an InvalidOperationException
       // if you try to add a node that already belongs to a list.
-      Console.WriteLine("Test 10: Throw exception by adding node (fox) already in the list:");
+      Console.WriteLine("\nPrueba 10: Lanzar excepción al añadir un nodo existente (es) already in the list:");
       try {
         oracion.AddBefore(nodoActual, nodo1);
       }
-      catch (InvalidOperationException ex) {
-        Console.WriteLine("Exception message: {0}", ex.Message);
+      catch (InvalidOperationException error) {
+        Console.WriteLine("Mensaje de excepción: {0}", error.Message);
       }
       Console.WriteLine();
 
@@ -84,80 +85,72 @@ namespace _LinkedList {
       // Indicate the node referred to by nodoActual.
       oracion.Remove(nodo1);
       oracion.AddBefore(nodoActual, nodo1);
-      IndicateNode(nodoActual, "Test 11: Move a referenced node (fox) before the nodoActual node (dog):");
+      BuscarNodo(nodoActual, "\nPrueba 11: Mover un nodo referenciado (es) a una posición anterior a nodoActual (¿?):");
 
       // Remove the node referred to by nodoActual.
       oracion.Remove(nodoActual);
-      IndicateNode(nodoActual, "Test 12: Remove nodoActual node (dog) and attempt to indicate it:");
+      BuscarNodo(nodoActual, "\nPrueba 12: Borrar nodoActual (¿?) e intentar indicarlo:");
 
-      // Add the node after the node referred to by mark2.
-      oracion.AddAfter(mark2, nodoActual);
-      IndicateNode(nodoActual, "Test 13: Add node removed in test 11 after a referenced node (brown):");
+      // Add the node after the node referred to by nodo2.
+      oracion.AddAfter(nodo2, nodoActual);
+      BuscarNodo(nodoActual, "\nPrueba 13: Añadir el nodo borrado en la Prueba 11 a una posición posterior (¿?):");
 
       // The Remove method finds and removes the
       // first node that that has the specified value.
-      oracion.Remove("old");
-      Display(oracion, "Test 14: Remove node that has the value 'old':");
+      oracion.Remove("Animal");
+      Muestrame(oracion, "\nPrueba 14: Borrar nodo 'Animal':");
 
-      // When the linked list is cast to ICollection(Of String),
+      // When the linked list is cast to ICollection(of String),
       // the Add method adds a node to the end of the list.
       oracion.RemoveLast();
-      ICollection<string> icoll = oracion;
-      icoll.Add("rhinoceros");
-      Display(oracion, "Test 15: Remove last node, cast to ICollection, and add 'rhinoceros':");
+      ICollection<string> coleccion = oracion;
+      coleccion.Add("Rinoceronte");
+      Muestrame(oracion, "\nPrueba 15: Borrar el último nodo, enviar a ICollection, y añadir 'Rinoceronte':");
 
-      Console.WriteLine("Test 16: Copy the list to an array:");
+      Console.WriteLine("\nPrueba 16: Copiar la lista a un vector:");
       // Create an array with the same number of
       // elements as the linked list.
-      string[] sArray = new string[oracion.Count];
-      oracion.CopyTo(sArray, 0);
-
-      foreach (string s in sArray) {
-        Console.WriteLine(s);
-      }
+      string[] vector = new string[oracion.Count];
+      oracion.CopyTo(vector, 0);
+      foreach (string cadena in vector) Console.WriteLine(cadena);
 
       // Release all the nodes.
       oracion.Clear();
 
       Console.WriteLine();
-      Console.WriteLine("Test 17: Clear linked list. Contains 'jumps' = {0}",
-          oracion.Contains("jumps"));
+      Console.WriteLine("\nPrueba 17: Limpiar la lista enlazada: ¿Contiene 'Gerardo'? = {0}", oracion.Contains("Gerardo"));
 
       Console.ReadLine();
     }
 
-    private static void Display(LinkedList<string> palabras, string test) {
-      Console.WriteLine(test);
-      foreach (string word in palabras) {
-        Console.Write(word + " ");
-      }
-      Console.WriteLine();
+    private static void Muestrame(LinkedList<string> palabras, string prueba) {
+      Console.WriteLine(prueba);
+      foreach (string palabra in palabras) Console.Write(palabra + " ");
       Console.WriteLine();
     }
 
-    private static void IndicateNode(LinkedListNode<string> node, string test) {
-      Console.WriteLine(test);
-      if (node.List == null) {
-        Console.WriteLine("Node '{0}' is not in the list.\n",
-            node.Value);
+    private static void BuscarNodo(LinkedListNode<string> nodo, string prueba) {
+      Console.WriteLine(prueba);
+      if (nodo.List == null) {
+        Console.WriteLine("El nodo '{0}' no está en la lista.\n", nodo.Value);
         return;
       }
 
-      StringBuilder result = new StringBuilder("(" + node.Value + ")");
-      LinkedListNode<string> nodeP = node.Previous;
+      StringBuilder resultado = new StringBuilder("*" + nodo.Value + "*");
+      LinkedListNode<string> nodoAnterior = nodo.Previous;
 
-      while (nodeP != null) {
-        result.Insert(0, nodeP.Value + " ");
-        nodeP = nodeP.Previous;
+      while (nodoAnterior != null) {
+        resultado.Insert(0, nodoAnterior.Value + " ");
+        nodoAnterior = nodoAnterior.Previous;
       }
 
-      node = node.Next;
-      while (node != null) {
-        result.Append(" " + node.Value);
-        node = node.Next;
+      nodo = nodo.Next;
+      while (nodo != null) {
+        resultado.Append(" " + nodo.Value);
+        nodo = nodo.Next;
       }
 
-      Console.WriteLine(result);
+      Console.WriteLine(resultado);
       Console.ReadKey();
     }
   }
