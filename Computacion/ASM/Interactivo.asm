ad7293623559260adee070dfdd0de0e744832ac9
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-06-10 20:14:37 -0300

    Salto y Retorno

--- Computacion/ASM/Interactivo.asm
+++ Computacion/ASM/Interactivo.asm
@@ -1,102 +1,121 @@
-INCLUDE 'emu8086.inc'   ; Biblioteca de funciones comunes (ej.: PRINT).
-
+INCLUDE 'emu8086.inc'   ; Biblioteca de funciones comunes (por ejemplo: PRINT).
 ORG 100h
+; La directiva ORG 100H indica  al ensamblador  que continue  el ensamblado a
+; partir de la direccion dada por el argumento, en este caso 100H.
 
-; // *--------------------------------------------------------------=> VARIABLES
+; //////////////////////////////////////////////////////////////////// VARIABLES
 
-cociente DB ?
-resto    DB ?
+cociente DB ?           ; DB stays for Define Byte.
+resto    DB ?           ;
 
-; // *---------------------------------------------------------------=> PROGRAMA
+; ///////////////////////////////////////////////////////////////////// PROGRAMA
 
-LEA     SI, numero1     ; Pedir 1er nro
-CALL    print_string    ; LLamado a libreria (procedimiento de emu8086.inc)
-CALL    scan_num        ; Poner numero en CX (procedimiento de emu8086.inc)
-MOV     DX, CX          ; copy the number to AX
-CALL    pthis
-DB      13, 10, 0
+LEA     si, pedido1     ;
+; Pedir primer numero. (LEA) Load Effective Address: store effective address for
+; pedido1 in register si (source index): LEA loads a pointer to the item you're
+; addressing.
 
-LEA     SI, numero2     ; Pedir 2do nro
+CALL    print_string
+; recibe la direccion en el registro si para imprimirla, es decir, imprime el
+; pedido1
+
+CALL    scan_num        ; recibe el numero por teclado y lo aloja en cx
+MOV     dx, cx          ; copia en dx lo guardado en cx (1er numero)
+CALL    pthis           ; imprime la cadena en la posicion del cursor
+DB      13, 10, 0       ; 13: CR / 10: LF / 0: Terminator for the string.
+
+LEA     si, pedido2     ; Pedir 2do nro
 CALL    print_string    ; LLamado a libreria (procedimiento de emu8086.inc)
 CALL    scan_num        ; LLamado a libreria (procedimiento de emu8086.inc)
-MOV     BX, CX
+MOV     bx, cx          ; Copiar numero en bx
 CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
-DB      13, 10, 0
+DB      13, 10, 0       ; imprimir salto de linea y retorno de carro
 
 PRINT   '[1(+), 2(-), 3(*), 4(/)]?: '
 CALL    scan_num        ; LLamado a libreria (procedimiento de emu8086.inc)
-MOV     AX, 2
-CMP     AX, CX
+MOV     ax, 2           ;
+CMP     ax, cx          ;
 
-JE      l2              ; Jump if Equal (=)
-JL      l3              ; Jump if Less (<)
-JG      l1              ; Jump if Greater (>)
-  
-l1: ADD   DX, BX
-    CALL  pthis         ; LLamado a libreria (procedimiento de emu8086.inc)
-    DB    13, 10, 'Suma(+): ', 0
-    MOV   AX, DX
-    CALL  print_num     ; LLamado a libreria (procedimiento de emu8086.inc)
-    RET
+JE      restar          ; Jump if Equal (=)
+JL      multiplicar     ; Jump if Less (<)
+JG      sumar           ; Jump if Greater (>)
 
-l2: SUB   DX, BX
-    CALL  pthis         ; LLamado a libreria (procedimiento de emu8086.inc)
-    DB    13, 10, 'Resta(-): ', 0
-    MOV   AX, DX
-    CALL  print_num     ; LLamado a libreria (procedimiento de emu8086.inc)
-    RET
+sumar: ; //-----------------------------------------------------------------=> *
 
-l3: MOV   AX, 3
-    CMP   AX, CX
-    JNE   l6            ; Jump if Not Equal (<>).
-    MOV   AX, DX
-    MUL   BX
-    CALL  pthis         ; LLamado a libreria (procedimiento de emu8086.inc)
-    DB    13, 10, 'Multiplicacion(*): ', 0
-    CALL  print_num     ; LLamado a libreria (procedimiento de emu8086.inc)
-    RET
+ADD     dx, bx          ;
+CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
+DB      13, 10, 'Suma(+): ', 0
+MOV     ax, dx          ;
+CALL    print_num       ; LLamado a libreria (procedimiento de emu8086.inc)
+RET                     ;
 
-l6: MOV   AX, DX
-    MOV   BL, 5
-    DIV   BL
-    MOV   cociente, AL
-    MOV   resto, AH
-    MOV   DL, cociente
-    ADD   DL, 48
-    MOV   AH, 2
-    CALL  pthis
-    DB    10, 13, 0
-    PRINT "Division(/): "
-    INT   21h
-    MOV   DL, resto
-    ADD   DL, 48
-    MOV   AH, 2
-    PRINT ". Resto: "
-    INT   21h
-    RET
+restar: ; //----------------------------------------------------------------=> *
 
-numero1 DB 'Ingrese el 1er numero: ', 0
-numero2 DB 'Ingrese el 2do numero: ', 0          
+SUB     dx, bx          ;
+CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
+DB      13, 10, 'Resta(-): ', 0
+MOV     ax, dx          ;
+CALL    print_num       ; LLamado a libreria (procedimiento de emu8086.inc)
+RET                     ;
 
-; // *-----------------------------------------------------------=> DEFINICIONES
-; Fuente: https://jbwyatt.com/253/emu/asm_tutorial_05.html
+multiplicar: ; //-----------------------------------------------------------=> *
 
-DEFINE_SCAN_NUM         ; procedure that gets the multi-digit SIGNED number from
-                        ; the keyboard, and stores the result in CX register.
+MOV     ax, 3           ;
+CMP     ax, cx          ;
+JNE     dividir         ; Jump if Not Equal (<>).
+MOV     ax, dx          ;
+MUL     bx              ;
+CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
+DB      13, 10, 'Multiplicacion(*): ', 0
+CALL    print_num       ; LLamado a libreria (procedimiento de emu8086.inc)
+RET                     ;
 
-DEFINE_PRINT_STRING     ; procedure to print a null terminated string at current
-                        ; cursor position, receives address of string in DS:SI
-                        ; register.
+dividir: ; //---------------------------------------------------------------=> *
+
+MOV     ax, dx          ;
+MOV     bl, 5           ;
+DIV     bl              ;
+MOV     cociente, al    ;
+MOV     resto, ah       ;
+MOV     dl, cociente    ;
+ADD     dl, 48          ;
+MOV     ah, 2           ;
+CALL    pthis           ;
+DB      10, 13, 0       ;
+PRINT   "Division(/): "
+INT     21h             ;
+MOV     dl, resto       ;
+ADD     dl, 48          ;
+MOV     ah, 2           ;
+PRINT   ". Resto: "
+INT     21h             ;
+RET                     ;
+; //------------------------------------------------------------------------=> *
+
+pedido1 DB 'Ingrese el 1er numero: ', 0
+pedido2 DB 'Ingrese el 2do numero: ', 0          
+
+; ///////////////////////////////////////////////////////////////// DEFINICIONES
+
+DEFINE_SCAN_NUM         
+; procedure that gets the multi-digit SIGNED number from the keyboard, and
+; stores the result in CX register.
+
+DEFINE_PRINT_STRING     
+; procedure to print a null terminated string at current cursor position,
+; receives address of string in DS:SI register.
 
 DEFINE_PRINT_NUM        ; procedure that prints a signed number in AX register.
 
 DEFINE_PRINT_NUM_UNS    ; required for print_num.
 
-DEFINE_PTHIS            ; procedure to print a null terminated string at current
-                        ; cursor position (just as PRINT_STRING).
-                        ; The ZERO TERMINATED string should be defined just
-                        ; after the CALL. For example:
-                        ;       CALL PTHIS
-                        ;       db 'Hello World!', 0
+DEFINE_PTHIS            
+; procedure to print a null terminated string at current cursor position (just
+; as PRINT_STRING). The ZERO TERMINATED string should be defined just after the
+; CALL. For example:
+;                       CALL PTHIS
+;                       db 'Hello World!', 0
+
+; // *----------------------------------------------------------------------=> *
 
 END
