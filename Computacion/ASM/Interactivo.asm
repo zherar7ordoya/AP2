aba1a8ef6b29947230ee9e403588f13e4539fcd1
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-06-10 12:14:06 -0300

    Interactivo

--- Computacion/ASM/Interactivo.asm
+++ Computacion/ASM/Interactivo.asm
@@ -3,77 +3,77 @@ ORG 100h
 
 ; VARIABLES.---------------------------
 cociente DB ?
-resto DB ?
+resto    DB ?
 
 ; PROGRAMA.----------------------------
-LEA SI, numero1       ; pedir 1er nro
-CALL print_string
-CALL scan_num      ; get number in CX
-MOV DX, CX         ; copy the number to AX
-CALL pthis
-DB 13, 10, 0
+LEA     SI, numero1       ; pedir 1er nro
+CALL    print_string
+CALL    scan_num      ; get number in CX
+MOV     DX, CX         ; copy the number to AX
+CALL    pthis
+DB      13, 10, 0
 
-LEA SI, numero2       ; pedir 2do nro
-CALL print_string
-CALL scan_num
-MOV BX, CX
-CALL pthis
-DB 13, 10, 0
+LEA     SI, numero2       ; pedir 2do nro
+CALL    print_string
+CALL    scan_num
+MOV     BX, CX
+CALL    pthis
+DB      13, 10, 0
 
-PRINT '1(+), 2(-), 3(*), 4(/): '
-CALL scan_num
-MOV AX, 2
-CMP AX, CX
+PRINT   '[1(+), 2(-), 3(*), 4(/)]?: '
+CALL    scan_num
+MOV     AX, 2
+CMP     AX, CX
 
-je l2
-jl l3
-jg l1
+JE      l2 ; Jump if Equal (=)
+JL      l3 ; Jump if Less (<)
+JG      l1 ; Jump if Greater (>)
   
-l1: ADD DX, BX
-    CALL pthis
-    DB 13, 10, '[sumando...] > Resultado: ', 0
-    MOV AX, DX
-    CALL print_num
+l1: ADD   DX, BX
+    CALL  pthis
+    DB    13, 10, 'Suma(+): ', 0
+    MOV   AX, DX
+    CALL  print_num
     RET
 
-l2: SUB DX, BX
-    CALL pthis
-    DB 13, 10, '[restando...] > Resultado: ', 0
-    MOV AX, DX
-    CALL print_num
+l2: SUB   DX, BX
+    CALL  pthis
+    DB    13, 10, 'Resta(-): ', 0
+    MOV   AX, DX
+    CALL  print_num
     RET
 
-l3: MOV AX, 3
-    CMP AX, CX
-    jne l6
-    MOV AX, DX
-    MUL BX
-    CALL pthis
-    DB 13, 10, '[multiplicando...] > Resultado: ', 0
-    CALL print_num
+l3: MOV   AX, 3
+    CMP   AX, CX
+    JNE   l6 ; Jump if Not Equal (<>).
+    MOV   AX, DX
+    MUL   BX
+    CALL  pthis
+    DB    13, 10, 'Multiplicacion(*): ', 0
+    CALL  print_num
     RET
 
-l6: MOV AX, DX
-    MOV BL, 5
-    DIV BL
-    MOV cociente, AL
-    MOV resto, AH
-    MOV DL, cociente
-    ADD DL, 48
-    MOV AH, 2
-    call pthis
-    DB 10, 13, 0
-    print "[dividiendo...] > Resultado: "
-    INT 21h
-    MOV DL, resto
-    ADD DL, 48
-    MOV AH, 2
-    print ", (resto): "
-    INT 21h
+l6: MOV   AX, DX
+    MOV   BL, 5
+    DIV   BL
+    MOV   cociente, AL
+    MOV   resto, AH
+    MOV   DL, cociente
+    ADD   DL, 48
+    MOV   AH, 2
+    CALL  pthis
+    DB    10, 13, 0
+    PRINT "Division(/): "
+    INT   21h
+    MOV   DL, resto
+    ADD   DL, 48
+    MOV   AH, 2
+    PRINT ". Resto: "
+    INT   21h
     RET
 
-numero1 DB 'Ingrese el 1er nro: ', 0
-numero2 DB 'Ingrese el 2do nro: ', 0          
+numero1 DB 'Ingrese el 1er numero: ', 0
+numero2 DB 'Ingrese el 2do numero: ', 0          
 
 DEFINE_SCAN_NUM
 DEFINE_PRINT_STRING
