57715bbc0c32ec9f280c1e66b481ef8834630dfa
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-06-10 13:14:59 -0300

    Interactivo2

--- Computacion/ASM/Interactivo.asm
+++ Computacion/ASM/Interactivo.asm
@@ -1,56 +1,59 @@
-INCLUDE 'emu8086.inc'
+INCLUDE 'emu8086.inc'   ; Biblioteca de funciones comunes (ej.: PRINT).
+
 ORG 100h
 
-; VARIABLES.---------------------------
+; // *--------------------------------------------------------------=> VARIABLES
+
 cociente DB ?
 resto    DB ?
 
-; PROGRAMA.----------------------------
-LEA     SI, numero1       ; pedir 1er nro
-CALL    print_string
-CALL    scan_num      ; get number in CX
-MOV     DX, CX         ; copy the number to AX
+; // *---------------------------------------------------------------=> PROGRAMA
+
+LEA     SI, numero1     ; Pedir 1er nro
+CALL    print_string    ; LLamado a libreria (procedimiento de emu8086.inc)
+CALL    scan_num        ; Poner numero en CX (procedimiento de emu8086.inc)
+MOV     DX, CX          ; copy the number to AX
 CALL    pthis
 DB      13, 10, 0
 
-LEA     SI, numero2       ; pedir 2do nro
-CALL    print_string
-CALL    scan_num
+LEA     SI, numero2     ; Pedir 2do nro
+CALL    print_string    ; LLamado a libreria (procedimiento de emu8086.inc)
+CALL    scan_num        ; LLamado a libreria (procedimiento de emu8086.inc)
 MOV     BX, CX
-CALL    pthis
+CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
 DB      13, 10, 0
 
 PRINT   '[1(+), 2(-), 3(*), 4(/)]?: '
-CALL    scan_num
+CALL    scan_num        ; LLamado a libreria (procedimiento de emu8086.inc)
 MOV     AX, 2
 CMP     AX, CX
 
-JE      l2 ; Jump if Equal (=)
-JL      l3 ; Jump if Less (<)
-JG      l1 ; Jump if Greater (>)
+JE      l2              ; Jump if Equal (=)
+JL      l3              ; Jump if Less (<)
+JG      l1              ; Jump if Greater (>)
   
 l1: ADD   DX, BX
-    CALL  pthis
+    CALL  pthis         ; LLamado a libreria (procedimiento de emu8086.inc)
     DB    13, 10, 'Suma(+): ', 0
     MOV   AX, DX
-    CALL  print_num
+    CALL  print_num     ; LLamado a libreria (procedimiento de emu8086.inc)
     RET
 
 l2: SUB   DX, BX
-    CALL  pthis
+    CALL  pthis         ; LLamado a libreria (procedimiento de emu8086.inc)
     DB    13, 10, 'Resta(-): ', 0
     MOV   AX, DX
-    CALL  print_num
+    CALL  print_num     ; LLamado a libreria (procedimiento de emu8086.inc)
     RET
 
 l3: MOV   AX, 3
     CMP   AX, CX
-    JNE   l6 ; Jump if Not Equal (<>).
+    JNE   l6            ; Jump if Not Equal (<>).
     MOV   AX, DX
     MUL   BX
-    CALL  pthis
+    CALL  pthis         ; LLamado a libreria (procedimiento de emu8086.inc)
     DB    13, 10, 'Multiplicacion(*): ', 0
-    CALL  print_num
+    CALL  print_num     ; LLamado a libreria (procedimiento de emu8086.inc)
     RET
 
 l6: MOV   AX, DX
@@ -75,10 +78,25 @@ l6: MOV   AX, DX
 numero1 DB 'Ingrese el 1er numero: ', 0
 numero2 DB 'Ingrese el 2do numero: ', 0          
 
-DEFINE_SCAN_NUM
-DEFINE_PRINT_STRING
-DEFINE_PRINT_NUM
-DEFINE_PRINT_NUM_UNS    ;required for print_num
-DEFINE_PTHIS
+; // *-----------------------------------------------------------=> DEFINICIONES
+; Fuente: https://jbwyatt.com/253/emu/asm_tutorial_05.html
+
+DEFINE_SCAN_NUM         ; procedure that gets the multi-digit SIGNED number from
+                        ; the keyboard, and stores the result in CX register.
+
+DEFINE_PRINT_STRING     ; procedure to print a null terminated string at current
+                        ; cursor position, receives address of string in DS:SI
+                        ; register.
+
+DEFINE_PRINT_NUM        ; procedure that prints a signed number in AX register.
+
+DEFINE_PRINT_NUM_UNS    ; required for print_num.
+
+DEFINE_PTHIS            ; procedure to print a null terminated string at current
+                        ; cursor position (just as PRINT_STRING).
+                        ; The ZERO TERMINATED string should be defined just
+                        ; after the CALL. For example:
+                        ;       CALL PTHIS
+                        ;       db 'Hello World!', 0
 
 END
