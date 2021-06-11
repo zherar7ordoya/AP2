e35affc2722d667a9464fad72202e9c2c162e490
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-06-11 19:57:19 -0300

    Maquillaje

--- Computacion/ASM/Calculin.asm
+++ Computacion/ASM/Calculin.asm
@@ -12,26 +12,26 @@ ORG 100h
 
 ; //////////////////////////////////////////////////////////////////// VARIABLES
 
-cociente DB ?           ; DB stays for Define Byte.
-resto    DB ?           ; Signo de interrogacion: variable no inicializada.
+cocte   DB ?            ; DB stays for Define Byte.
+resto   DB ?            ; Signo de interrogacion: variable no inicializada.
 
 ; ///////////////////////////////////////////////////////////////////// PROGRAMA
 
-LEA     si, pedido1
+LEA     si, txt1
 ; Pedir primer numero. (LEA) Load Effective Address: store effective address for
-; pedido1 in register si (source index): LEA loads a pointer to the item you're
+; txt1 in register SI (source index): LEA loads a pointer to the item you're
 ; addressing.
 
 CALL    print_string
-; recibe la direccion en el registro si para imprimirla, es decir, imprime el
-; pedido1 (es un puntero, bah!)
+; recibe la direccion en el registro SI para imprimirla, es decir, imprime el
+; txt1 (es un puntero, bah!)
 
 CALL    scan_num        ; recibe el numero por teclado y lo aloja en cx
 MOV     dx, cx          ; copia en dx lo guardado en cx (1er numero)
 CALL    pthis           ; imprime la cadena en la posicion del cursor
 DB      13, 10, 0       ; 13: CR / 10: LF / 0: Terminator for the string.
 
-LEA     si, pedido2     ; Pedir 2do nro
+LEA     si, txt2        ; Pedir 2do nro
 CALL    print_string    ; LLamado a libreria (procedimiento de emu8086.inc)
 CALL    scan_num        ; LLamado a libreria (procedimiento de emu8086.inc)
 MOV     bx, cx          ; Copiar numero en bx
@@ -42,9 +42,23 @@ DB      13, 10, 0       ; imprimir salto de linea y retorno de carro (CRLF)
 ;   1er numero en dx y 2do en bx.
 ;   en cx ha quedado el 2do numero (puesto ahi originalmente por scan).
 
-PRINT   '[1(+), 2(-), 3(*), 4(/)]?: '
+CALL    pthis           ; Algo de cosmetica...
+DB      10, 13, 0       ; ...un salto de linea
+PRINTN  'Ingrese el numero de la operacion a realizar:'
+CALL    pthis           ; Algo de cosmetica...
+DB      10, 13, 0       ; ...un salto de linea
+PRINTN  '1) Suma'
+PRINTN  '2) Resta'
+PRINTN  '3) Multiplicacion'
+PRINTN  '4) Division'
+CALL    pthis           ; Algo de cosmetica...
+DB      10, 13, 0       ; ...un salto de linea
+
 CALL    scan_num        ; scan aloja (por ej.) un 4 en cx
 MOV     ax, 2           ; copiamos un 2 en ax
+
+CALL    pthis           ; Algo de cosmetica...
+DB      10, 13, 0       ; ...un salto de linea
 
 CMP     ax, cx          ; en cx esta el nro de operacion (4 en nuestro ejemplo).
 ; Veamos un ejemplo para entender a CMP: "SUB cl, dl" resta de cl el valor de dl
@@ -78,7 +92,7 @@ sumar: ; //-----------------------------------------------------------------=> *
 
 ADD     dx, bx          ; 1er numero en dx y 2do en bx.
 CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
-DB      13, 10, 'Suma(+): ', 0
+DB      13, 10, 'El total de la suma es ', 0
 MOV     ax, dx          ; print_num imprime lo que hay en ax (no en dx)
 CALL    print_num       ; LLamado a libreria (procedimiento de emu8086.inc)
 RET                     ; salida del programa
@@ -87,7 +101,7 @@ restar: ; //----------------------------------------------------------------=> *
 
 SUB     dx, bx          ; 1er numero en dx y 2do en bx.
 CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
-DB      13, 10, 'Resta(-): ', 0
+DB      13, 10, 'El total de la resta es ', 0
 
 MOV     ax, dx
 ; el resultado se copia en ax. EXPLICACION: segun la libreria, "This macro
@@ -127,8 +141,8 @@ MUL     bx
 ; a ser procesada alli (y alli tambien se almacenara el resultado).
 
 CALL    pthis           ; A ver, lo aclaremos: imprime la siguiente cadena...
-DB      13, 10, 'Multiplicacion(*): ', 0                    ; ...esta cadena
-CALL    print_num       ; procedimiento que imprime un nÃºmero en el registro AX.
+DB      13, 10, 'El total de la multiplicacion es ', 0          ; ...esta cadena
+CALL    print_num       ; procedimiento que imprime un numero en el registro AX.
 RET                     ; regreso (retorno) al sistema operativo (SO).
 
 dividir: ; //---------------------------------------------------------------=> *
@@ -139,21 +153,21 @@ MOV     ax, dx          ; copia en ax el 1er numero.
 DIV     bl
 ; Para la division, el dividendo ha de estar en AX, y ser de 16 bits. El divisor
 ; se indica en el operando, por ejemplo: "DIV BL". Este divisor estaria en BL.
-; Se dividira AX entre BL y el cociente quedara en AL y el resto en AH.
-MOV     cociente, al    ; segun lo explicado: en "cociente" el cociente.
+; Se dividira AX entre BL y el cocte quedara en AL y el resto en AH.
+MOV     cocte, al    ; segun lo explicado: en "cocte" el cocte.
 MOV     resto, ah       ; segun lo explicado: en "resto" el resto.
 
 ; * <=----------------------------------------------------------------------=> *
 ; IMPRIMIR UNA DIVISION. Todo un tema. Al quedar "divido" el resultado (en AL el
-; cociente y en AH el resto), el print_num no funcionar . As¡ que hay que operar
+; cocte y en AH el resto), el print_num no funcionara. Asi que hay que operar.
 
-MOV     dl, cociente    ; copia el valor "real" de cociente
+MOV     dl, cocte       ; copia el valor "real" de cocte
 ADD     dl, 48          ; 48 es el cero en la tabla ASCII
 MOV     ah, 2           ; este 2 sera el codigo de operacion para INT 21h
 
-CALL    pthis           ; imprime lo que viene a continuacion
+CALL    pthis           ; imprime lo que viene a continuacion (un CRLF)
 DB      10, 13, 0       ; salto de linea
-PRINT   'Division(/): '
+PRINT   'El total de la division es '
 
 INT     21h
 ; En ah tenemos 2 (02h). "INT 21h" es una interrupcion del DOS (es decir, se 
@@ -161,20 +175,21 @@ INT     21h
 ; operacion cuyo codigo esta en ah. Y actualmente, ah=02h.
 ; Codigo de que es la operacion "02h"?:
 ;   AH = 02h > WRITE CHARACTER TO STANDARD OUTPUT
+
 ; >>>>>>>>>> Es decir: imprime el equivalente ASCII del codigo en DL <<<<<<<<<<<
 
-; ************ A PARTIR DE AQUI, PARA EL COCIENTE, SE REPITE EL PROCESO ANTERIOR 
+; ************ A PARTIR DE AQUI, PARA EL cocte, SE REPITE EL PROCESO ANTERIOR 
 MOV     dl, resto       ; copia el valor "real" de resto
 ADD     dl, 48          ; suma 48 (0 en ASCII) a dl
 MOV     ah, 2           ; la funcion 02h escribe al dispositivo de salida
-PRINT   " [Resto: "
+PRINT   ' (con '
 INT     21h             ; imprime DL (en nuestro ejemplo, 52 ASCII = 4)
-PRINT   "]"
+PRINT   ' como resto)'
 ; ******************************************************************************
 RET                     ; regresa al DOS
 
-pedido1 DB 'Ingrese el 1er numero: ', 0
-pedido2 DB 'Ingrese el 2do numero: ', 0          
+txt1     DB 'Ingrese un numero: ', 0
+txt2     DB 'Ingrese otro numero: ', 0          
 
 ; ///////////////////////////////////////////////////////////////// DEFINICIONES
 
