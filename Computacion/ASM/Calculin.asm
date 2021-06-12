761b08296297adbc177e310d1d399888e27dd13f
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-06-12 10:32:43 -0300

    Comments

--- Computacion/ASM/Calculin.asm
+++ Computacion/ASM/Calculin.asm
@@ -1,112 +1,69 @@
-; @author:  Gerardo Tordoya
-; @date:    2021-06-11
+; @author:      Gerardo Tordoya
+; @date:        2021-06-12
+; @references:  Footnotes
 
-INCLUDE 'emu8086.inc'
-; Biblioteca de funciones comunes (por ejemplo: PRINT).
-; La lectura de la biblioteca es muy clarificadora. Puede consultarse en:
-; github.com/AhmadNaserTurnkeySolutions/emu8086/blob/master/inc/emu8086.inc
-
-ORG 100h
-; La directiva ORG 100H indica  al ensamblador  que continue  el ensamblado a
-; partir de la direccion dada por el argumento, en este caso 100H.
+INCLUDE 'emu8086.inc'   ; [1]
+ORG 100h                ; [2]
 
 ; //////////////////////////////////////////////////////////////////// VARIABLES
 
-cocte   DB ?            ; DB stays for Define Byte.
+cocient DB ?            ; DB stays for Define Byte.
 resto   DB ?            ; Signo de interrogacion: variable no inicializada.
 
 ; ///////////////////////////////////////////////////////////////////// PROGRAMA
 
-LEA     si, txt1
-; Pedir primer numero. (LEA) Load Effective Address: store effective address for
-; txt1 in register SI (source index): LEA loads a pointer to the item you're
-; addressing.
-
-CALL    print_string
-; recibe la direccion en el registro SI para imprimirla, es decir, imprime el
-; txt1 (es un puntero, bah!)
-
-CALL    scan_num        ; recibe el numero por teclado y lo aloja en cx
-MOV     dx, cx          ; copia en dx lo guardado en cx (1er numero)
-CALL    pthis           ; imprime la cadena en la posicion del cursor
+LEA     si, txt1        ; [3]
+CALL    print_string    ; [4]
+CALL    scan_num        ; Recibe el numero por teclado y lo aloja en CX
+MOV     dx, cx          ; Copia en DX lo guardado en CX (1er numero)
+CALL    pthis           ; Imprime la cadena en la posicion del cursor
 DB      13, 10, 0       ; 13: CR / 10: LF / 0: Terminator for the string.
-
+; REPITE LOGICA DEL 1ER BLOQUE.-------------------------------------------------
 LEA     si, txt2        ; Pedir 2do nro
 CALL    print_string    ; LLamado a libreria (procedimiento de emu8086.inc)
 CALL    scan_num        ; LLamado a libreria (procedimiento de emu8086.inc)
 MOV     bx, cx          ; Copiar numero en bx
 CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
 DB      13, 10, 0       ; imprimir salto de linea y retorno de carro (CRLF)
+; PRUEBA DE ESCRITORIO.---------------------------------------------------------
+;   1er numero en DX, 2do numero en BX.
+;   Scan dejo en CX ha quedado el 2do numero (puesto ahi originalmente por scan).
+CALL    pthis           ; Imprimir lo siguiente:
+DB      10, 13, 0       ; CRLF
+PRINTN  'Codigo de operacion: '
+CALL    pthis           ; Imprimir lo siguiente:
+DB      10, 13, 0       ; CRLF
 
-; PRUEBA DE ESCRITORIO:
-;   1er numero en dx y 2do en bx.
-;   en cx ha quedado el 2do numero (puesto ahi originalmente por scan).
+PRINTN  '  1) Suma'
+PRINTN  '  2) Resta'
+PRINTN  '  3) Multiplicacion'
+PRINTN  '  4) Division'
 
-CALL    pthis           ; Algo de cosmetica...
-DB      10, 13, 0       ; ...un salto de linea
-PRINTN  'Ingrese el numero de la operacion a realizar:'
-CALL    pthis           ; Algo de cosmetica...
-DB      10, 13, 0       ; ...un salto de linea
-PRINTN  '1) Suma'
-PRINTN  '2) Resta'
-PRINTN  '3) Multiplicacion'
-PRINTN  '4) Division'
-CALL    pthis           ; Algo de cosmetica...
-DB      10, 13, 0       ; ...un salto de linea
+CALL    pthis           ; Imprimir lo siguiente:
+DB      10, 13, 0       ; CRLF
+CALL    scan_num        ; Scan aloja el codigo de operacion en CX
+MOV     ax, 2           ; Copiamos un 2 en AX
+CALL    pthis           ; Imprimir lo siguiente:
+DB      10, 13, 0       ; CRLF
 
-CALL    scan_num        ; scan aloja (por ej.) un 4 en cx
-MOV     ax, 2           ; copiamos un 2 en ax
-
-CALL    pthis           ; Algo de cosmetica...
-DB      10, 13, 0       ; ...un salto de linea
-
-CMP     ax, cx          ; en cx esta el nro de operacion (4 en nuestro ejemplo).
-; Veamos un ejemplo para entender a CMP: "SUB cl, dl" resta de cl el valor de dl
-; y almacena el resultado en cl.
-; CMP tiene el mismo formato que SUB (por ejemplo: CMP ax, bx > es decir: ax-bx)
-; tan solo que ninguno de los registros es alterado. Si por ejemplo son iguales,
-; el flag de cero se pondra en uno.
-; En realidad, CMP es un SUB que no almacena el resultado.
-
+CMP     ax, cx          ; [5] [6]
 JE      restar          ; Jump if Equal (=)
 JL      multiplicar     ; Jump if Less (<)
 JG      sumar           ; Jump if Greater (>)
-; Los saltos (jump), en este caso, actuan como si se tratasen de un switch. La
-; pregunta seria como lo hacen. En ensamblador, los CMP están ligados a los
-; JUMP para hacer justamente eso:
-; CMP hizo una comparacion. Esa comparacion, realizada en el procesador, alineo
-; los flags. Y los JUMP leen justamente eso para realizar los saltos: los flags.
-; Entonces:
-;       JE: cuando ZF = 1
-;       JL: cuando SF <> OF
-;       JG: cuando ZF = 0 y SF = OF
-; Luego de la resta, los flags pertinentes:
-;       ZF (zero flag): Set if result is zero; cleared otherwise.
-;       SF (sign flag): Set equal to high-order bit of result
-;                       (0 if positive 1 if negative).
-;       OF (overflow flag): Set if result is too large a positive number or too
-;                           small a negative number (excluding sign bit) to fit
-;                           in destination operand; cleared otherwise.
 
-sumar: ; //-----------------------------------------------------------------=> *
-
+sumar:                  ; ------------------------------------------------------
 ADD     dx, bx          ; 1er numero en dx y 2do en bx.
 CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
-DB      13, 10, 'El total de la suma es ', 0
+DB      13, 10, 'SUMA = ', 0
 MOV     ax, dx          ; print_num imprime lo que hay en ax (no en dx)
 CALL    print_num       ; LLamado a libreria (procedimiento de emu8086.inc)
 RET                     ; salida del programa
 
-restar: ; //----------------------------------------------------------------=> *
-
+restar:                 ; ------------------------------------------------------
 SUB     dx, bx          ; 1er numero en dx y 2do en bx.
 CALL    pthis           ; LLamado a libreria (procedimiento de emu8086.inc)
-DB      13, 10, 'El total de la resta es ', 0
-
-MOV     ax, dx
-; el resultado se copia en ax. EXPLICACION: segun la libreria, "This macro
-; defines a procedure that prints number in AX".
-
+DB      13, 10, 'RESTA = ', 0
+MOV     ax, dx          ; [7]
 CALL    print_num       ; LLamado a libreria (procedimiento de emu8086.inc)
 RET                     ; regreso a DOS
 
@@ -141,7 +98,7 @@ MUL     bx
 ; a ser procesada alli (y alli tambien se almacenara el resultado).
 
 CALL    pthis           ; A ver, lo aclaremos: imprime la siguiente cadena...
-DB      13, 10, 'El total de la multiplicacion es ', 0          ; ...esta cadena
+DB      13, 10, 'MULTIPLICACION = ', 0          ; ...esta cadena
 CALL    print_num       ; procedimiento que imprime un numero en el registro AX.
 RET                     ; regreso (retorno) al sistema operativo (SO).
 
@@ -153,21 +110,21 @@ MOV     ax, dx          ; copia en ax el 1er numero.
 DIV     bl
 ; Para la division, el dividendo ha de estar en AX, y ser de 16 bits. El divisor
 ; se indica en el operando, por ejemplo: "DIV BL". Este divisor estaria en BL.
-; Se dividira AX entre BL y el cocte quedara en AL y el resto en AH.
-MOV     cocte, al    ; segun lo explicado: en "cocte" el cocte.
+; Se dividira AX entre BL y el cocient quedara en AL y el resto en AH.
+MOV     cocient, al    ; segun lo explicado: en "cocient" el cocient.
 MOV     resto, ah       ; segun lo explicado: en "resto" el resto.
 
 ; * <=----------------------------------------------------------------------=> *
 ; IMPRIMIR UNA DIVISION. Todo un tema. Al quedar "divido" el resultado (en AL el
-; cocte y en AH el resto), el print_num no funcionara. Asi que hay que operar.
+; cocient y en AH el resto), el print_num no funcionara. Asi que hay que operar.
 
-MOV     dl, cocte       ; copia el valor "real" de cocte
+MOV     dl, cocient       ; copia el valor "real" de cocient
 ADD     dl, 48          ; 48 es el cero en la tabla ASCII
 MOV     ah, 2           ; este 2 sera el codigo de operacion para INT 21h
 
 CALL    pthis           ; imprime lo que viene a continuacion (un CRLF)
 DB      10, 13, 0       ; salto de linea
-PRINT   'El total de la division es '
+PRINT   'DIVISION = '
 
 INT     21h
 ; En ah tenemos 2 (02h). "INT 21h" es una interrupcion del DOS (es decir, se 
@@ -178,18 +135,17 @@ INT     21h
 
 ; >>>>>>>>>> Es decir: imprime el equivalente ASCII del codigo en DL <<<<<<<<<<<
 
-; ************ A PARTIR DE AQUI, PARA EL cocte, SE REPITE EL PROCESO ANTERIOR 
+; ************ A PARTIR DE AQUI, PARA EL cocient, SE REPITE EL PROCESO ANTERIOR 
 MOV     dl, resto       ; copia el valor "real" de resto
 ADD     dl, 48          ; suma 48 (0 en ASCII) a dl
 MOV     ah, 2           ; la funcion 02h escribe al dispositivo de salida
-PRINT   ' (con '
+PRINT   ' || RESTO = '
 INT     21h             ; imprime DL (en nuestro ejemplo, 52 ASCII = 4)
-PRINT   ' como resto)'
 ; ******************************************************************************
 RET                     ; regresa al DOS
 
-txt1     DB 'Ingrese un numero: ', 0
-txt2     DB 'Ingrese otro numero: ', 0          
+txt1     DB 'Ingrese  un  numero: ', 0
+txt2     DB 'Ingrese otro numero: ', 0       
 
 ; ///////////////////////////////////////////////////////////////// DEFINICIONES
 
@@ -215,3 +171,53 @@ DEFINE_PTHIS
 ; // *----------------------------------------------------------------------=> *
 
 END
+
+; [1]
+; Biblioteca de funciones comunes (por ejemplo: PRINT).
+; La lectura de la biblioteca es muy clarificadora. Puede consultarse en:
+; github.com/AhmadNaserTurnkeySolutions/emu8086/blob/master/inc/emu8086.inc
+
+; [2]
+; La directiva ORG 100H indica  al ensamblador  que continue  el ensamblado a
+; partir de la direccion dada por el argumento, en este caso 100H.
+
+; [3]
+; Pedir primer numero. (LEA) Load Effective Address: store effective address for
+; txt1 in register SI (source index): LEA loads a pointer to the item you're
+; addressing.
+
+; [4]
+; recibe la direccion en el registro SI para imprimirla, es decir, imprime el
+; txt1 (es un puntero, bah!)
+
+; [5] 
+; En CX esta el codigo de operacion.
+; Veamos un ejemplo para entender a CMP: "SUB cl, dl" resta de cl el valor de dl
+; y almacena el resultado en cl.
+; CMP tiene el mismo formato que SUB (por ejemplo: CMP ax, bx > es decir: ax-bx)
+; tan solo que ninguno de los registros es alterado. Si por ejemplo son iguales,
+; el flag de cero se pondra en uno.
+; En realidad, CMP es un SUB que no almacena el resultado.
+
+; [6]
+; Los saltos (jump), en este caso, actuan como si se tratasen de un switch. La
+; pregunta seria como lo hacen. En ensamblador, los CMP estan ligados a los
+; JUMP para hacer justamente eso:
+; CMP hizo una comparacion. Esa comparacion, realizada en el procesador, alineo
+; los flags. Y los JUMP leen justamente eso para realizar los saltos: los flags.
+; Entonces:
+;       JE: cuando ZF = 1
+;       JL: cuando SF <> OF
+;       JG: cuando ZF = 0 y SF = OF
+; Luego de la resta, los flags pertinentes:
+;       ZF (zero flag): Set if result is zero; cleared otherwise.
+;       SF (sign flag): Set equal to high-order bit of result
+;                       (0 if positive 1 if negative).
+;       OF (overflow flag): Set if result is too large a positive number or too
+;                           small a negative number (excluding sign bit) to fit
+;                           in destination operand; cleared otherwise.
+
+; [7]
+; el resultado se copia en ax. EXPLICACION: segun la libreria, "This macro
+; (print_num) defines a procedure that prints number in AX", y por eso hay que
+; copiar el valor de DX en AX.
