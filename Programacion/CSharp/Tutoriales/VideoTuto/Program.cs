7911383dd127db8f9e4c8af1321179f2ae7e886a
Gerardo Tordoya <gtordoya@TRIBJUY.local>
2021-06-16 19:53:13 -0300

    VideoTuto

---
+++ Programacion/CSharp/Tutoriales/VideoTuto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace VideoTuto {
+  class Program {
+    static void Main() {
+      /* VIDEO 64
+      StreamWriter escritura = File.CreateText("ejemplo.txt");
+      escritura.WriteLine("Bienvenido a la Pachangona");
+      escritura.Close();
+      */
+      /* VIDEO 65
+      string respuesta, cadena;
+      respuesta = "";
+      cadena = "";
+      StreamWriter escribir = File.AppendText("datos.txt");
+      do {
+        Console.Write("Ingresa texto: ");
+        cadena = Console.ReadLine();
+        escribir.WriteLine(cadena);
+        Console.Write("¿Seguir escribiendo? (SI/NO): ");
+        respuesta = Console.ReadLine();
+        respuesta = respuesta.ToUpper();
+      } while (respuesta.Equals("SI"));
+      escribir.Close();
+      */
+
+    }
+  }
+}
