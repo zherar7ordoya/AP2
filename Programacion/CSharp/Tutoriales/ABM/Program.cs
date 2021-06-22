5404c5dd2462b712c75a08bf7f091e52abfee5a5
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-06-22 00:19:30 -0300

    TP10

--- Programacion/CSharp/Tutoriales/ABM/Program.cs
+++ Programacion/CSharp/Tutoriales/ABM/Program.cs
@@ -46,12 +46,16 @@ namespace ABM {
               Altas();
               break;
             case 2:
+              Bajas();
               break;
             case 3:
+              Consultas();
               break;
             case 4:
+              Modificaciones();
               break;
             case 5:
+              ConsultaGeneral();
               break;
             case 6:
               Console.WriteLine("APLICACIÓN FINALIZADA");
@@ -127,7 +131,7 @@ namespace ABM {
             color       + ", " +
             precio
             );
-          Console.WriteLine("Registro agregado correctamente.");
+          Console.WriteLine("Registro agregado correctamente.");               
         }
         else { Console.WriteLine("Ya existe nro de serie.");  }
         Escritura.Close();
@@ -139,5 +143,256 @@ namespace ABM {
         Escritura.Close();
       }
     }
+
+    // BAJAS
+    static void Bajas() {
+      encontrado = false;
+
+      try {
+        Lectura = File.OpenText("autos.txt");
+        temporal = File.CreateText("tmp.txt");
+
+        Console.WriteLine("Número de serie: ");
+        noSerie = Console.ReadLine();
+        noSerie = noSerie.ToUpper();
+        cadena = Lectura.ReadLine();
+
+        while(cadena!=null) {
+          campos = cadena.Split(',');
+
+          if(campos[0].Trim().Equals(noSerie)) {
+
+            encontrado = true;
+            Console.WriteLine("Serie:       " + campos[0]);
+            Console.WriteLine("Modelo:      " + campos[1]);
+            Console.WriteLine("Año:         " + campos[2]);
+            Console.WriteLine("Fabricante:  " + campos[3]);
+            Console.WriteLine("Color:       " + campos[4]);
+            Console.WriteLine("Precio:      " + campos[5]);
+            Console.WriteLine("Eliminar?");
+            respuesta = Console.ReadLine();
+            respuesta = respuesta.ToUpper();
+
+            if (!respuesta.Equals("SI")) { temporal.WriteLine(cadena); }
+            
+          }
+          else { temporal.WriteLine(cadena); }
+
+          cadena = Lectura.ReadLine();
+        }
+
+        if(encontrado==false) { Console.WriteLine("El nro de serie " + noSerie + "no existe en la BBDD."); }
+        else if(respuesta.Equals("SI")) { Console.WriteLine("Auto eliminado."); }
+        else { Console.WriteLine("Eliminación cancelada.");  }
+
+        Lectura.Close();
+        temporal.Close();
+        File.Delete("autos.txt");
+        File.Move("tmp.txt", "autos.txt");
+
+      }
+      catch (FileNotFoundException fn) { Console.WriteLine("Error: " + fn.Message); }
+      catch (Exception e) { Console.WriteLine("Error: " + e.Message); }
+      finally {
+        Lectura.Close();
+        temporal.Close();
+      }
+    }
+
+    // CONSULTAS
+    static void Consultas() {
+      encontrado = false;
+
+      try {
+        Lectura = File.OpenText("autos.txt");
+        Console.Write("Fabricante: ");
+        fabricante = Console.ReadLine();
+        fabricante = fabricante.ToUpper();
+        cadena = Lectura.ReadLine();
+        while(cadena!=null) {
+          campos = cadena.Split(',');
+
+          if (campos[3].Trim().Equals(fabricante)) {
+            encontrado = true;
+            Console.WriteLine("Serie:       " + campos[0]);
+            Console.WriteLine("Modelo:      " + campos[1]);
+            Console.WriteLine("Año:         " + campos[2]);
+            Console.WriteLine("Fabricante:  " + campos[3]);
+            Console.WriteLine("Color:       " + campos[4]);
+            Console.WriteLine("Precio:      " + campos[5]);
+            Console.WriteLine("-------------------------");
+          }
+          cadena = Lectura.ReadLine();
+        }
+        if (encontrado==false) { Console.WriteLine("Fabricante no encontrado"); }
+        Lectura.Close();
+      }
+      catch (FileNotFoundException fn) { Console.WriteLine("Error: " + fn.Message); }
+      catch (Exception e) { Console.WriteLine("Error: " + e.Message); }
+      finally { Lectura.Close(); }
+    }
+
+
+    // MODIFICACIONES
+    static void Modificaciones() {
+      encontrado = false;
+      byte opcionM = 0;
+      string nuevoModelo, nuevoFabricante, nuevoColor;
+      short nuevoAnio;
+      double nuevoPrecio;
+
+      try {
+        Lectura = File.OpenText("autos.txt");
+        temporal = File.CreateText("tmp.txt");
+
+        Console.Write("Nro de serie: ");
+        noSerie = Console.ReadLine();
+        noSerie = noSerie.ToUpper();
+        cadena = Lectura.ReadLine();
+
+        while(cadena!=null) {
+          campos = cadena.Split(',');
+
+          if(campos[0].Trim().Equals(noSerie)) {
+            encontrado = true;
+            Console.WriteLine("Serie:       " + campos[0]);
+            Console.WriteLine("Modelo:      " + campos[1]);
+            Console.WriteLine("Año:         " + campos[2]);
+            Console.WriteLine("Fabricante:  " + campos[3]);
+            Console.WriteLine("Color:       " + campos[4]);
+            Console.WriteLine("Precio:      " + campos[5]);
+            Console.WriteLine("-------------------------");
+
+            Console.Write("Confirmar (SI/NO):");
+            respuesta = Console.ReadLine();
+            respuesta = respuesta.ToUpper();
+
+            if (respuesta.Equals("SI")) {
+              Console.WriteLine("Menú de opciones a modificar: ");
+              Console.WriteLine("    1. Modelo");
+              Console.WriteLine("    2. Año");
+              Console.WriteLine("    3. Fabricante");
+              Console.WriteLine("    4. Color");
+              Console.WriteLine("    5. Precio");
+              Console.WriteLine("¿Qué desea modificar");
+
+              opcionM = Convert.ToByte(Console.ReadLine());
+
+              switch(opcionM) {
+                case 1:
+                  Console.Write("Modelo: ");
+                  nuevoModelo = Console.ReadLine();
+                  nuevoModelo = nuevoModelo.ToUpper();
+                  temporal.WriteLine(
+                    campos[0]   + ',' +
+                    nuevoModelo + ',' +
+                    campos[2]   + ',' +
+                    campos[3]   + ',' +
+                    campos[4]   + ',' +
+                    campos[5]   + ',');
+                  Console.WriteLine("Modelo modificado.");
+                  break;
+                case 2:
+                  Console.Write("Año: ");
+                  nuevoAnio = Convert.ToInt16(Console.ReadLine());
+                  temporal.WriteLine(
+                    campos[0] + ',' +
+                    campos[1] + ',' +
+                    nuevoAnio + ',' +
+                    campos[3] + ',' +
+                    campos[4] + ',' +
+                    campos[5] + ',');
+                  Console.WriteLine("Año modificado.");
+                  break;
+                case 3:
+                  Console.Write("Fabricante: ");
+                  nuevoFabricante = Console.ReadLine();
+                  nuevoFabricante = nuevoFabricante.ToUpper();
+                  temporal.WriteLine(
+                    campos[0]       + ',' +
+                    campos[1]       + ',' +
+                    campos[2]       + ',' +
+                    nuevoFabricante + ',' +
+                    campos[4]       + ',' +
+                    campos[5]       + ',');
+                  Console.WriteLine("Fabricante modificado.");
+                  break;
+                case 4:
+                  Console.Write("Color: ");
+                  nuevoColor = Console.ReadLine();
+                  nuevoColor = nuevoColor.ToUpper();
+                  temporal.WriteLine(
+                    campos[0]   + ',' +
+                    campos[1]   + ',' +
+                    campos[2]   + ',' +
+                    campos[3]   + ',' +
+                    nuevoColor  + ',' +
+                    campos[5]   + ',');
+                  Console.WriteLine("Color modificado.");
+                  break;
+                case 5:
+                  Console.Write("Precio: ");
+                  nuevoPrecio = Convert.ToDouble(Console.ReadLine());
+                  temporal.WriteLine(
+                    campos[0]   + ',' +
+                    campos[1]   + ',' +
+                    campos[2]   + ',' +
+                    campos[3]   + ',' +
+                    campos[4]   + ',' +
+                    nuevoPrecio + ',');
+                  Console.WriteLine("Precio modificado.");
+                  break;
+                default:
+                  Console.WriteLine("Opción incorrecta");
+                  break;
+              }
+            } else { temporal.WriteLine(cadena); }
+          } else { temporal.WriteLine(cadena); }
+          cadena = Lectura.ReadLine();
+        }
+
+        if(encontrado==false) { Console.WriteLine("Registro no encontrado"); }
+        Lectura.Close();
+        temporal.Close();
+        File.Delete("autos.txt");
+        File.Move("tmp.txt", "autos.txt");
+
+      }
+      catch (FileNotFoundException fn) { Console.WriteLine("Error: " + fn.Message); }
+      catch (Exception e) { Console.WriteLine("Error: " + e.Message); }
+      finally {
+        Lectura.Close();
+        temporal.Close();
+      }
+    }
+
+    // CONSULTAS GENERALES
+    static void ConsultaGeneral() {
+      encontrado = false;
+
+      try {
+        Lectura = File.OpenText("autos.txt");
+        cadena = Lectura.ReadLine();
+
+        while(cadena!=null) {
+          encontrado = true;
+          campos = cadena.Split(',');
+          Console.WriteLine("Serie:       " + campos[0]);
+          Console.WriteLine("Modelo:      " + campos[1]);
+          Console.WriteLine("Año:         " + campos[2]);
+          Console.WriteLine("Fabricante:  " + campos[3]);
+          Console.WriteLine("Color:       " + campos[4]);
+          Console.WriteLine("Precio:      " + campos[5]);
+          Console.WriteLine("-------------------------");
+          cadena = Lectura.ReadLine();
+        }
+        if (encontrado==false) { Console.WriteLine("Registro no encontrado."); }
+        Lectura.Close();
+      }
+      catch (FileNotFoundException fn) { Console.WriteLine("Error: " + fn.Message); }
+      catch (Exception e) { Console.WriteLine("Error: " + e.Message); }
+      finally { Lectura.Close(); }
+    }
+
   }
 }
