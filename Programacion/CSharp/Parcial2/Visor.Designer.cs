a961b3447bda4dfc3a2ae5d347ed6c079b79f60c
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-07-10 23:32:29 -0300

    Parcial 2 Programación 1

--- Programacion/CSharp/Parcial2/Visor.Designer.cs
+++ Programacion/CSharp/Parcial2/Visor.Designer.cs
@@ -29,29 +29,11 @@ namespace Parcial2
         /// </summary>
         private void InitializeComponent()
         {
-            this.grillaAntes = new System.Windows.Forms.DataGridView();
-            this.grillaDespues = new System.Windows.Forms.DataGridView();
             this.lblAntes = new System.Windows.Forms.Label();
             this.lblDespues = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.grillaAntes)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.grillaDespues)).BeginInit();
+            this.grillaDespues = new System.Windows.Forms.ListView();
+            this.grillaAntes = new System.Windows.Forms.ListView();
             this.SuspendLayout();
-            // 
-            // grillaAntes
-            // 
-            this.grillaAntes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grillaAntes.Location = new System.Drawing.Point(12, 25);
-            this.grillaAntes.Name = "grillaAntes";
-            this.grillaAntes.Size = new System.Drawing.Size(326, 150);
-            this.grillaAntes.TabIndex = 0;
-            // 
-            // grillaDespues
-            // 
-            this.grillaDespues.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grillaDespues.Location = new System.Drawing.Point(12, 232);
-            this.grillaDespues.Name = "grillaDespues";
-            this.grillaDespues.Size = new System.Drawing.Size(326, 150);
-            this.grillaDespues.TabIndex = 1;
             // 
             // lblAntes
             // 
@@ -65,36 +47,52 @@ namespace Parcial2
             // lblDespues
             // 
             this.lblDespues.AutoSize = true;
-            this.lblDespues.Location = new System.Drawing.Point(12, 216);
+            this.lblDespues.Location = new System.Drawing.Point(427, 9);
             this.lblDespues.Name = "lblDespues";
             this.lblDespues.Size = new System.Drawing.Size(273, 13);
             this.lblDespues.TabIndex = 3;
             this.lblDespues.Text = "Listado de personas que ingresan después del mediodía";
             // 
+            // grillaDespues
+            // 
+            this.grillaDespues.HideSelection = false;
+            this.grillaDespues.Location = new System.Drawing.Point(430, 25);
+            this.grillaDespues.Name = "grillaDespues";
+            this.grillaDespues.Size = new System.Drawing.Size(370, 200);
+            this.grillaDespues.TabIndex = 4;
+            this.grillaDespues.UseCompatibleStateImageBehavior = false;
+            // 
+            // grillaAntes
+            // 
+            this.grillaAntes.HideSelection = false;
+            this.grillaAntes.Location = new System.Drawing.Point(15, 25);
+            this.grillaAntes.Name = "grillaAntes";
+            this.grillaAntes.Size = new System.Drawing.Size(370, 200);
+            this.grillaAntes.TabIndex = 5;
+            this.grillaAntes.UseCompatibleStateImageBehavior = false;
+            // 
             // Parcial2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(354, 400);
+            this.ClientSize = new System.Drawing.Size(813, 237);
+            this.Controls.Add(this.grillaAntes);
+            this.Controls.Add(this.grillaDespues);
             this.Controls.Add(this.lblDespues);
             this.Controls.Add(this.lblAntes);
-            this.Controls.Add(this.grillaDespues);
-            this.Controls.Add(this.grillaAntes);
             this.Name = "Parcial2";
             this.Text = "Parcial 2";
-            ((System.ComponentModel.ISupportInitialize)(this.grillaAntes)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.grillaDespues)).EndInit();
+            this.Load += new System.EventHandler(this.Parcial2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView grillaAntes;
-        private System.Windows.Forms.DataGridView grillaDespues;
         private System.Windows.Forms.Label lblAntes;
         private System.Windows.Forms.Label lblDespues;
+        private System.Windows.Forms.ListView grillaDespues;
+        private System.Windows.Forms.ListView grillaAntes;
     }
 }
 
---
+++ Programacion/CSharp/Parcial2/Visor.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Parcial2
+{
+    public partial class Parcial2 : Form
+    {
+        public Parcial2()
+        {
+            InitializeComponent();
+        }
+        private void Parcial2_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            PrepararArchivo("../../empleados.txt");
+            PrepararArchivo("../../ingresos.txt");
+            AntesMediodia();    // Vista izquierda
+            DespuesMediodia();  // Vista derecha
+        }
+
+        #region COMUNES
+        private void PrepararArchivo(string archivo)
+        {
+            string contenido = File.ReadAllText(archivo);
+            var lineas = contenido.Split(Environment.NewLine.ToCharArray()).ToList();
+            var ordenado = lineas.OrderBy(linea => linea.Split(',')[0]).ToList();
+            int inicio = 0, final = ordenado.Count - 1;
+
+            while (inicio < final && String.IsNullOrWhiteSpace(ordenado[inicio])) inicio++;
+            while (final >= inicio && String.IsNullOrWhiteSpace(ordenado[final])) final--;
+
+            var limpiado = ordenado.Skip(inicio).Take(final - inicio + 1);
+            File.WriteAllText(archivo, string.Join(Environment.NewLine, limpiado));
+        }
+        #endregion
+
+        #region IZQUIERDO
+        private void AntesMediodia()
+        {
+            string[] empleados = File.ReadAllLines(@"../../empleados.txt");
+            string[] ingresos =  File.ReadAllLines(@"../../ingresos.txt") ;
+            int hora = 12;
+            string intercambio = "../../temporal.txt";
+
+            IEnumerable<string> resultados = from empleado in empleados
+                                             let registroEmpleado = empleado.Split(',')
+                                             from ingreso in ingresos
+                                             let registroIngreso = ingreso.Split(',')
+                                             where Convert.ToInt32(registroEmpleado[0]) == Convert.ToInt32(registroIngreso[3])
+                                             where Convert.ToInt32(registroIngreso[0]) < hora
+                                             select registroEmpleado[0] + "\t" +
+                                                    registroIngreso[0]  + "\t" +
+                                                    registroIngreso[1]  + "\t" +
+                                                    registroIngreso[2]  + "\t" +
+                                                    registroEmpleado[1] + "\t" +
+                                                    registroEmpleado[2];
+
+            File.WriteAllText(intercambio, string.Join(Environment.NewLine, resultados));
+            string[] individuales = File.ReadAllLines(intercambio);
+
+            var agrupados = from fila in individuales
+                            let registro = fila.Split('\t')
+                            group registro by registro[0] into REGISTROS
+                            select new
+                            {
+                                Legajo = REGISTROS.Key,
+                                Promedio = REGISTROS.Average(registro => Convert.ToInt32(registro[1])),
+                            };
+
+            grillaAntes.View = View.Details;
+            grillaAntes.Columns.Add("Legajo");
+            grillaAntes.Columns.Add("Promedio");
+
+            foreach (var agrupado in agrupados)
+            {
+                grillaAntes.Items.Add(new ListViewItem(new string[] {
+                                                                      agrupado.Legajo,
+                                                                      agrupado.Promedio.ToString()
+                }));
+            }
+        }
+        #endregion
+
+        #region DERECHO
+        private void DespuesMediodia()
+        {
+            string[] empleados = File.ReadAllLines(@"../../empleados.txt");
+            string[] ingresos =  File.ReadAllLines(@"../../ingresos.txt") ;
+            int hora = 12;
+
+            IEnumerable<string> resultados = from empleado in empleados
+                                             let registroEmpleado = empleado.Split(',')
+                                             from ingreso in ingresos
+                                             let registroIngreso = ingreso.Split(',')
+                                             where Convert.ToInt32(registroEmpleado[0]) == Convert.ToInt32(registroIngreso[3])
+                                             where Convert.ToInt32(registroIngreso[0]) > hora
+                                             select registroEmpleado[0] + "\t" +
+                                                    registroEmpleado[1] + "\t" +
+                                                    registroEmpleado[2] + "\t" +
+                                                    registroIngreso[0]  + "\t" +
+                                                    registroIngreso[1]  + "\t" +
+                                                    registroIngreso[2];
+            grillaDespues.View = View.Details;
+            grillaDespues.Columns.Add("Legajo");
+            grillaDespues.Columns.Add("Nombre");
+            grillaDespues.Columns.Add("Apellido");
+            grillaDespues.Columns.Add("Hora");
+            grillaDespues.Columns.Add("Minuto");
+            grillaDespues.Columns.Add("Segundo");
+
+            string[] campos;
+
+            foreach (string resultado in resultados)
+            {
+                campos = resultado.Split('\t');
+                grillaDespues.Items.Add(new ListViewItem(new string[] {
+                                                                        campos[0],
+                                                                        campos[1],
+                                                                        campos[2],
+                                                                        campos[3],
+                                                                        campos[4],
+                                                                        campos[5]
+                }));
+            }
+        }
+        #endregion
+    
+    }
+}
