9640341a6c5db27f83d9d3c4b3be523463833ab3
Gerardo Tordoya <zherar7ordoya@outlook.com>
2021-08-03 22:19:45 -0300

    Vuelta Final

---
+++ Programacion/CSharp/Integrador/Cola.cs
@@ -0,0 +1,48 @@
+﻿namespace Integrador
+{
+    class Cola
+    {
+        Nodo _inicio;
+        public int Contador = 0;
+        public void Encolar(Nodo unNodo)
+        {
+            if (_inicio==null)
+            {
+                _inicio = unNodo;
+            }
+            else
+            {
+                Nodo auxiliar = BuscarUltimo(_inicio);
+                auxiliar.Siguiente = unNodo;
+            }
+            Contador++;
+        }
+        public void Desencolar()
+        {
+            _inicio = _inicio.Siguiente;
+            Contador--;
+        }
+        private Nodo BuscarUltimo(Nodo unNodo)
+        {
+            if (unNodo.Siguiente == null)
+            {
+                return unNodo;
+            }
+            else
+            {
+                return BuscarUltimo(unNodo.Siguiente);
+            }
+        }
+        public bool Vacia()
+        {
+            return (_inicio == null);
+        }
+        public Nodo Inicio
+        {
+            get
+            {
+                return _inicio;
+            }
+        }
+    }
+}
---
+++ Programacion/CSharp/Integrador/Nodo.cs
@@ -0,0 +1,8 @@
+﻿namespace Integrador
+{
+    class Nodo
+    {
+        public string Nombre { get; set; }
+        public Nodo Siguiente { get; set; }
+    }
+}
